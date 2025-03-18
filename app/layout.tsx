53528c8ce264e27ee5a1abbfcbe94d953217dbb4
tesol2y090 <gliese090@gmail.com>
2025-03-19 00:59:16 +0700

    feat: add recaptchar route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Script from 'next/script'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Akave Faucet",
-  description: "Claim AKVF tokens",
+  title: 'Akave Faucet',
+  description: 'Claim AKVF tokens',
   icons: {
     icon: [
       {
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
       },
     ],
   },
-};
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script src={`https://www.google.com/recaptcha/api.js?render=explicit`} async defer />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
